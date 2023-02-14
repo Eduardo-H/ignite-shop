c4922d4257f67ce2d3eb006ed23a9f4facdd3636
Eduardo-H <edu-henrique2014@hotmail.com>
2023-02-14 10:17:37 -0300

    Global styles configured

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,7 @@
 import { styled } from '@/styles';
 
-export const Button = styled('button', {
-  backgroundColor: '$rocketseat',
-  border: 0,
-  borderRadius: 6,
-  padding: '10px 20px',
-  cursor: 'pointer',
-  transition: 'filter 0.2s',
-
-  span: {
-    color: 'White',
-    fontWeight: 'bold'
-  },
-
-  '&:hover': {
-    filter: 'brightness(0.9)'
-  }
-});
-
 export default function Home() {
   return (
-    <>
-      <h1>Hello Next.js</h1>
-      <Button>
-        <span>Send e-mail</span>
-      </Button>
-    </>
+    <h1>Hello Next.js</h1>
   );
 }
