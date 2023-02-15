bae43210530cdf564b3f276a56ffb96a637b6cc6
Eduardo-H <edu-henrique2014@hotmail.com>
2023-02-14 23:05:50 -0300

    Home page implemented

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
+import Image from 'next/image';
 import type { AppProps } from 'next/app';
 import { globalStyles } from '@/styles/global';
+
 import { Container, Header } from '@/styles/pages/app';
 
 import logoImg from '../assets/logo.svg';
@@ -10,7 +12,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
-        <img src={logoImg.src} alt="Logo Ignite Shop" />
+        <Image src={logoImg} alt="Logo Ignite Shop" />
       </Header>
       
       <Component {...pageProps} />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,30 @@
-import { styled } from '@/styles';
+import Image from 'next/image';
+import { HomeContainer, Product } from '@/styles/pages/home';
+
+import shirt1 from '../assets/shirts/1.png';
+import shirt2 from '../assets/shirts/2.png';
+import shirt3 from '../assets/shirts/3.png';
 
 export default function Home() {
   return (
-    <h1>Hello Next.js</h1>
+    <HomeContainer>
+      <Product>
+        <Image src={shirt1} width={520} height={401} alt="" />
+
+        <footer>
+          <strong>T-Shirt Ignite Lab</strong>
+          <span>$19,90</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={shirt2} width={520} height={401} alt="" />
+
+        <footer>
+          <strong>T-Shirt Ignite Aboard</strong>
+          <span>$29,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   );
 }
